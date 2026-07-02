80d7a1fc96059c7fdfc5e8dbafbd7d0b7f69b245
GASS890 <289367169+GASS890@ users.noreply.github.com>
2026-07-02 13:28:04 +0900

    fix stylebert startup path

--- start_all_stylebert_ai.bat
+++ start_all_stylebert_ai.bat
@@ -2,7 +2,7 @@
 cd /d %~dp0
 
 echo Starting Style-Bert-VITS2 API...
-start "Style-Bert API" cmd /k "cd /d Style-Bert-VITS2 && python server_fastapi.py"
+start "Style-Bert API" cmd /k "cd /d tools\Style-Bert-VITS2 && python server_fastapi.py"
 
 timeout /t 5 /nobreak > nul
 
